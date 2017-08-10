e934b27288600dc1b715481260a3834150afa4ea
xaatw0 <xaatw0@gmail.com>
2017-08-10 11:50:09 +0900

    gradle runが実施できた

---
+++ src/main/java/com/ibuki101/admob/MyApplication.java
@@ -0,0 +1,23 @@
+package com.ibuki101.admob;
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class MyApplication extends Application {
+	  @Override
+	    public void start(Stage stage) {
+	        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+	        Scene scene = new Scene(new StackPane(new Label("Hello World!")),
+	                bounds.getWidth(), bounds.getHeight());
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+	  public static void main(String[] args) {
+		launch(args);
+	}
+}
